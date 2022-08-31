90b3cc5072c824f874159ecfbfbb13cfc34516d4
Nick <forizual@gmail.com>
2022-08-31 15:44:16 +0400

    add tablet Blog Page

--- src/pages/BlogPage/BlogPreviews/BlogPreviews.tsx
+++ src/pages/BlogPage/BlogPreviews/BlogPreviews.tsx
@@ -7,9 +7,11 @@ import styles from './blog-previews.module.scss'
 const BlogPreviews = () => {
   return (
     <Container className={styles.wrapper}>
-        {Array.from({ length: 6 }).map((_, i) => (
-            <BlogPreview key={i} />
-        ))}
+        <div className={styles.inner}>
+          {Array.from({ length: 6 }).map((_, i) => (
+              <BlogPreview key={i} />
+          ))}
+        </div>
 
         <Button className={styles.moreButton}>Load More</Button>
     </Container>
