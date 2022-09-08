9ad8fbf0e2b66b5221a3ad5d96c70865044c9c6a
zanedr <zanedanielrussell@gmail.com>
2022-09-08 16:47:01 -0600

    check load order

--- src/templates/BasicTemplate/BasicTemplateFooter/Subscription/Subscription.tsx
+++ src/templates/BasicTemplate/BasicTemplateFooter/Subscription/Subscription.tsx
@@ -13,6 +13,7 @@ const Subscription = () => {
     onSubmit: () => {}
   })
 
+  console.log('subscription')
   return (
     <div className={styles.wrapper}>
       <div className={styles.text}>Join our mailing list for<br/> updates on all things Lit</div>
