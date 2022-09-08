02e11a30873db10a144827c7ad56c9637b531e70
Nick <forizual@gmail.com>
2022-09-08 15:44:14 +0400

    add scrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
 import { renderPublicRoutes } from 'src/routing'
 
 const App = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
   return renderPublicRoutes()
 }
 
