447bf579cea6ad09d315cd84d47fd3c1b62c2fd9
zanedr <zanedanielrussell@gmail.com>
2023-02-24 13:00:49 -0700

    update w/ davids copy changes and alter landing cards to display multiple links

--- src/pages/LandingPage/LandingFeatures/LandingFeatures.tsx
+++ src/pages/LandingPage/LandingFeatures/LandingFeatures.tsx
@@ -8,25 +8,27 @@ const FEATURES = [
   {
     title: 'Programmable + Distributed Cloud Wallets',
     description:
-      'Lit provides distributed MPC wallets that can be used for DeFi automation, verifiable credential issuance, and users wallets that  web2 authentication (e.g Passkey, Discord oAuth).',
-    url: {
+      'Lit provides distributed MPC wallets that can be used for DeFi automation, verifiable credential issuance, and users wallets that  web2 authentication (e.g Passkey, Discord, oAuth).',
+    urls: [{
       label: 'Start With Programmable Key Pairs',
       href: 'https://developer.litprotocol.com/LitActionsAndPKPs/whatAreLitActionsAndPKPs',
-    },
+    }],
     image: Wallets,
   },
   {
     title: 'Encryption + Access Control',
     description:
       'Store private data on the open web with Lit as the access control layer to provision keys to users who meet your on-chain conditions for web3 social and data marketplaces.',
-    // url: {
-    //   label: 'Start With Encryption',
-    //   href: 'https://developer.litprotocol.com/ToolsAndExamples/SDKExamples/EncryptAndDecrypt/setup',
-    // },
-    url: {
-      label: 'Start With ACCESS CONTROL',
-      href: 'https://developer.litprotocol.com/AccessControlConditions/intro',
-    },
+    urls: [
+      {
+        label: 'Start With Encryption',
+        href: 'https://developer.litprotocol.com/ToolsAndExamples/SDKExamples/EncryptAndDecrypt/setup',
+      },
+      {
+        label: 'Start With ACCESS CONTROL',
+        href: 'https://developer.litprotocol.com/AccessControlConditions/intro',
+      }
+    ],
     image: Encryption,
     reverse: true,
   },
