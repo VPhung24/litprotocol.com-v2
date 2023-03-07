b6bae2cca7d1b6d86d9c6666a7d6810ac463fcb8
zanedr <zanedanielrussell@gmail.com>
2023-03-06 17:05:19 -0700

    add responsive mobile picture

--- src/pages/LandingPage/LandingNetwork/LandingNetwork.tsx
+++ src/pages/LandingPage/LandingNetwork/LandingNetwork.tsx
@@ -1,25 +1,28 @@
 import { Container } from 'src/components'
 
 import litFlowChart from './assets/litflowchart.png'
+import litFlowChartSmall from './assets/litflowchartsmall.png'
 
 import styles from './landing-network.module.scss'
 
 const LandingNetwork = () => {
   return (
     <div className={styles.wrapper}>
-        <Container innerClassName={styles.inner}>
-            <div className={styles.content}>
-                <h2 className={styles.title}>The Lit Network</h2>
+      <Container innerClassName={styles.inner}>
+        <div className={styles.content}>
+          <h2 className={styles.title}>The Lit Network</h2>
 
-                <div className={styles.subtitle}>
-                  Lit is composed of a decentralized network of nodes, each playing a role in key generation, consensus, and the execution of network actions.
-                </div>
-            </div>
-
-        </Container>
-        <div className={styles.readWriteImages}>
-          <img src={litFlowChart} alt="Cloud Signing" className={styles.readWriteImage} />
+          <div className={styles.subtitle}>
+            Lit is composed of a decentralized network of nodes, each playing a role in key generation, consensus, and
+            the execution of network actions.
+          </div>
         </div>
+
+      </Container>
+      <div className={styles.readWriteImages}>
+        <img src={litFlowChart} alt="Cloud Signing" className={styles.readWriteImage}/>
+        <img src={litFlowChartSmall} alt="Cloud Signing" className={styles.readWriteImageMobile}/>
+      </div>
     </div>
   )
 }
