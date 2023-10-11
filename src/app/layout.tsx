eb8fbff4254a337a83dde5a570d93ae31aab863a
sarahzdev <sarah.hg.zhang@gmail.com>
2023-10-11 15:55:56 -0700

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,29 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Lit Protocol',
   description:
-    'Lit is a distributed trust network for secure signing, encryption, and compute without centralized keyholders.',
+    'Lit is a distributed key management network for secure signing, encryption, and compute without centralized keyholders.',
+  openGraph: {
+    title: 'Lit Protocol',
+    description:
+      'Lit is a distributed key management network for secure signing, encryption, and compute without centralized keyholders.',
+    url: 'https://litprotocol.com',
+    siteName: 'Lit Protocol',
+    images: {
+      url: '/og-image.png',
+      alt: 'Description of Lit Protocol over a blue and orange gradient background',
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Lit Protocol',
+    description:
+      'Lit is a distributed key management network for secure signing, encryption, and compute without centralized keyholders.',
+    creator: '@LitProtocol',
+    images: {
+      url: '/twitter-image.png',
+      alt: 'Description of Lit Protocol over a blue and orange gradient background',
+    },
+  },
 };
 
 export default function RootLayout({
