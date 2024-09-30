82a2e9dd806e573da9186f1e1cb9941acd4a4dc0
Vivian Phung <dev+github@vivianphung.com>
2024-09-30 14:50:40 -0400

    remove mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,4 @@
-import remarkGfm from 'remark-gfm';
-import rehypePrettyCode from 'rehype-pretty-code';
-import createMDX from '@next/mdx';
-
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      [
-        rehypePrettyCode,
-        { keepBackground: false, grid: false, theme: 'slack-dark' },
-      ],
-    ],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    mdxRs: false,
-  },
-  webpack: (config, options) => {
-    // Merge the MDX configuration with the default webpack config
-    config = withMDX(config);
-
-    // You can add more custom webpack configurations here if needed
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
