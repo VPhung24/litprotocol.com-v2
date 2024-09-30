61366cbca74e41b64c41e7e3de64417128072647
Vivian Phung <dev+github@vivianphung.com>
2024-09-30 14:13:55 -0400

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,6 @@ import remarkGfm from 'remark-gfm';
 import rehypePrettyCode from 'rehype-pretty-code';
 import createMDX from '@next/mdx';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    mdxRs: false,
-  },
-};
-
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
@@ -23,4 +16,21 @@ const withMDX = createMDX({
     // providerImportSource: "@mdx-js/react",
   },
 });
-export default withMDX(nextConfig);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    mdxRs: false,
+  },
+  webpack: (config, options) => {
+    // Merge the MDX configuration with the default webpack config
+    config = withMDX(config);
+
+    // You can add more custom webpack configurations here if needed
+
+    return config;
+  },
+};
+
+export default nextConfig;
